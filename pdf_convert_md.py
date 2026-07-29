4453c7662737cfc6eccf628d0b253e3f55ab4617
Sum Sum Chan <sumsumdress@gmail.com>
2026-07-29 10:30:41 +0800

    convert compensation to pdf success output text

--- pdf_convert_md.py
+++ pdf_convert_md.py
@@ -1,6 +1,6 @@
 from docling.document_converter import DocumentConverter
 
-source = "./Riman Product Eng PDF.pdf"
+source = "./Compensation_plan_HK_CN.pdf"
 converter = DocumentConverter()
 result = converter.convert(source)
 print(result.document.export_to_markdown())
